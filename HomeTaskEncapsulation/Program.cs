1dd09585ed389e2e51e62ec4e351d58796a8e926
Kamranbb <kamrannb@code.edu.az>
2024-03-05 17:57:59 +0400

    Proje dosyası ekle.

---
+++ HomeTaskEncapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeTaskEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            //user.Age = 25;
+            //Console.WriteLine(user.Age);
+            user.Password = "kamran12";
+            Console.WriteLine(user.Password);
+        }
+    }
+}
---
+++ HomeTaskEncapsulation/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskEncapsulation
+{
+    internal class User
+    {
+        string _name;
+        string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Sifre bosluqlardan ibaret ola bilmez!");
+                    return;
+                }
+                else if (value.Length < 8)
+                {
+                    Console.WriteLine("Sifre 8 herf ve ya reqemden az olmamalidir!");
+                    return ;
+                }
+                _password = value;
+                
+            }
+
+        } 
+        int _age;
+
+        public int Age  { 
+            
+            get
+            {
+                return _age; 
+            }
+            
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Yasi duzgun daxil et ");
+                    return;
+                }
+                _age = value;   
+            }
+        
+        }
+    }
+}
